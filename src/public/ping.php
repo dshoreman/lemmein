8dbabb8d871c5e0e50284af7ee989cd132798912
Dave Shoreman <dshoreman@users.noreply.github.com>
2025-04-14 13:59:01 +0100

    Wrap select list in form, add Send Ping button

--- src/public/ping.php
+++ src/public/ping.php
@@ -17,20 +17,27 @@ try {
     <title>Ping | Lemmein</title>
     <style type="text/css">
       body { text-align: center; }
+      h2 { margin-top: 8vh; }
       select { font-size: large; padding: 1rem; }
-      select option { padding: 0.5rem 1rem; text-align: center; }
+      select option { padding: 1.5vh 3vw; text-align: center; }
+      button { font-size: larger; padding: 1vh 2vh; }
     </style>
   </head>
   <body>
     <h1>Device Ping</h1>
 
-    <select name="connection" id="connection" size="7">
-      <?php foreach ($list['connections'] as $connId => $conn): ?>
-      <option value="<?= $connId; ?>">
-        <?= $connId; ?> — <?= $conn['last_ip'] ?? 'No known IP'; ?>
-      </option>
-      <?php endforeach; ?>
-    </select>
-    <h2>Your IP: <?= $_SERVER['REMOTE_ADDR'] ?></h2>
+    <h2>Which Connection?</h2>
+    <form method="post" action="<?= htmlentities($_SERVER['PHP_SELF']); ?>">
+      <select name="connection" id="connection" size="7" required>
+        <?php foreach ($list['connections'] as $connId => $conn): ?>
+        <option value="<?= $connId; ?>">
+          <?= $connId; ?> — <?= $conn['last_ip'] ?? 'No known IP'; ?>
+        </option>
+        <?php endforeach; ?>
+      </select>
+
+      <h2>Your IP: <?= $_SERVER['REMOTE_ADDR'] ?></h2>
+      <button>Send Ping</button>
+    </form>
   </body>
 </html>
