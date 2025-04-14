d9c7a9bc07b1749a8d503089e3a71a8ecf84ba27
Dave Shoreman <dshoreman@users.noreply.github.com>
2025-04-14 21:41:25 +0100

    Allow static networks to be defined in lists

--- README.md
+++ README.md
@@ -9,9 +9,11 @@ Create a minimal **data/list.json** with the connections to be managed:
 ```json
 {
   "name": "Homelab Access",
+  "networks": {
+    "Home": "10.2.2.0/24",
+    "Work": "123.45.100.48/28"
+  },
   "connections": {
-    "Home": {},
-    "Work": {}
     "4G": {}
   }]
 }
--- src/public/list.php
+++ src/public/list.php
@@ -6,13 +6,17 @@ header('Content-Type: text/plain');
 
 try {
   $list = list_from_file();
+  $connections = array_values($list['connections']);
 
-  foreach ($list['connections'] as $name => $conn) {
-    if (!($conn['ip'] ?? null)) {
-      continue;
-    }
+  $entries = array_merge(
+    array_values($list['networks'] ?? []),
+    array_map(fn (array $conn): string => (
+      isset($conn['ip'], $conn['since']) ? $conn['ip'] : ''
+    ), $connections),
+  );
 
-    echo $conn['ip'] . PHP_EOL;
+  foreach (array_filter($entries) as $ip_or_cidr_block) {
+    echo $ip_or_cidr_block . PHP_EOL;
   }
 } catch (Exception $e) {
   echo 'Error: ' . $e->getMessage();
