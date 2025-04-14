59ac88fcfa537f40ec1d1209118f29dc2fb7974d
Dave Shoreman <dshoreman@users.noreply.github.com>
2025-04-14 03:02:54 +0100

    Show current device IP on separate ping page

---
+++ src/public/ping.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Ping | Lemmein</title>
+  </head>
+  <body>
+    <h1>Device Ping</h1>
+
+    <h2>Your IP: <?= $_SERVER['REMOTE_ADDR'] ?></h2>
+  </body>
+</html>
