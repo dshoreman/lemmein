2c3dc3b01d8b4d841393376aa06458a66cf75579
Dave Shoreman <dshoreman@users.noreply.github.com>
2025-04-23 13:32:52 +0100

    Add detailed 403 for list access from non-consumer

--- src/public/list.php
+++ src/public/list.php
@@ -1,15 +1,24 @@
 <?php
 
+$config = (object) [];
+
 header('Content-Type: text/plain');
 
 try {
   require_once '../core.php';
 
+  $ip = get_ip();
   $list = list_from_file();
   $consumers = $list['consumers'] ?? [];
 
-  if ($consumers && !in_array(get_ip(), $consumers)) {
-    die(http_response_code(403));
+  if ($consumers && !in_array($ip, $consumers)) {
+    http_response_code(403);
+
+    if ($config->detailed_denials) {
+      echo "IP {$ip} is not in consumers list.";
+    }
+
+    exit;
   }
 
   $connections = array_values($list['connections']);
